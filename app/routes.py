126198da6c63e31e04fd17060a239c45efbd169e
sysufzh <sysufzh@users.noreply.github.com>
2020-07-16 10:43:53 +0800

    first blog

---
+++ app/routes.py
@@ -0,0 +1,10 @@
+from app import app
+#从app模块中即从__init__.py中导入创建的app应用
+
+
+@app.route('/')
+@app.route('/index')
+#建立路由，通过路由可以执行其覆盖的方法，可以多个路由指向同一个方法。
+def index():
+    return "Hello, World!"
+    
