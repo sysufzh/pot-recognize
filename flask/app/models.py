1c26c7ce1ab5e179d427f94f6c5c6e95856bef86
sysufzh <sysufzh@users.noreply.github.com>
2020-07-17 16:50:20 +0800

    password

--- flask/app/models.py
+++ flask/app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -13,6 +14,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<用户名:{}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     __tablename__ = 'post'
