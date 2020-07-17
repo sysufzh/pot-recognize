0a0d9800a48674f5fc2bf1e21bcbe137d2e3001d
sysufzh <sysufzh@users.noreply.github.com>
2020-07-17 18:39:46 +0800

    add register

--- flask/app/form.py
+++ flask/app/form.py
@@ -9,3 +9,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('密码', validators=[DataRequired(message='请输入密码')])
     remember_me = BooleanField('记住我')
     submit = SubmitField('登录')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('用户名', validators=[DataRequired()])
+    email = StringField('邮箱', validators=[DataRequired(), Email()])
+    password = PasswordField('密码', validators=[DataRequired()])
+    password2 = PasswordField(
+        '重复密码', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('注册')
+    #校验用户名是否重复
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('用户名重复了，请您重新换一个呗!')
+    #校验邮箱是否重复
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('邮箱重复了，请您重新换一个呗!')
